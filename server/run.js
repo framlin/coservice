753d2ba6097a05c3faf6ffed234faec62d8da136
Wolfgang Egger <w.egger@framlin.com>
2018-01-11 13:14:14 +0100

    add comments with cors-restify-middleware usage

--- server/run.js
+++ server/run.js
@@ -3,6 +3,16 @@ var restify         = require('restify'),
     fs = require('fs');
 
 
+// const corsMiddleware = require('restify-cors-middleware');
+// const cors = corsMiddleware({
+//     origins: ['127.0.0.1:8080', '127.0.0.1:8081','127.0.0.1:8082', '127.0.0.1:8083', '127.0.0.1:8084', '127.0.0.1:8085',
+//         '127.0.0.1:8086', '127.0.0.1:8087', '127.0.0.1:8088', '127.0.0.1:8089', '127.0.0.1:8090', '127.0.0.1:8091',
+//         '127.0.0.1:8092', '127.0.0.1:8093', '127.0.0.1:8094', '127.0.0.1:8095'],
+//     credentials: true,
+//     headers: ['x-framlin-co']
+// });
+
+
 function coArticle(req, res, next) {
     fs.createReadStream(__dirname + '/../article/'+req.params.site+'/'+req.params.path+'.html').pipe(res);
 }
@@ -18,6 +28,11 @@ function coImage(req, res, next) {
 
 // ----------- run ----------------------------
 var server = restify.createServer();
+
+//---- new cors-restify-middleware usage
+// server.pre(cors.preflight);
+// server.use(cors.actual);
+
 
 // server.use(restify.CORS( {credentials: true, headers: ['x-framlin-co']}));
 // server.use(restify.fullResponse());
